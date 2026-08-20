1f9a6d60bc4ae0f9f47ba6ac836386c38edb80c8
karafi <simokrf02@gmail.com>
2026-08-20 18:41:21 +0100

    Initial setup of microservices infrastructure: Eureka, Config Server, and API Gateway

---
+++ init-db/init.sql
@@ -0,0 +1,4 @@
+CREATE DATABASE account_db;
+CREATE DATABASE payment_db;
+CREATE DATABASE fraud_db;
+CREATE DATABASE transaction_db;
---
+++ services/fraud-service/src/test/java/karafi/fraudservice/FraudServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package karafi.fraudservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FraudServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
